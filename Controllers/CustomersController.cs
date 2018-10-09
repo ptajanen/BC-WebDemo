e331e97867947e27181e9d546b1ed1a0a659daea
Pia Tajanen <ptajanen@gmail.com>
2018-10-09 14:42:38 +0300

    Lisätty entity-malli ja Controller

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BC_WebDemo.Controllers
+{
+    [Route("api/[controller]")]
+    public class CustomersController : Controller
+    {
+
+    }
+}
